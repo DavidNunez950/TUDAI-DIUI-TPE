fd8f6aaccb3df29b2013b51cb38df26191bed560
DavidNunez950 <davidlanuez@gmail.com>
2021-09-24 11:58:19 -0300

    RAgregado el código de las fichas, agregado un getter faltante

--- TP-2/js/token.js
+++ TP-2/js/token.js
@@ -11,8 +11,8 @@ export { Token }
 /** The Token class*/
 class Token {
 
-    #squareCoordinates = {x1:0, x2:0, y1:0, y2:0};
-    #playerName;
+    #squareCoordinates;
+    #playerColor;
     #image;
     #size;
     #used;
@@ -23,55 +23,72 @@ class Token {
     /**
      * Constructor
      * @param {SquareCoordinate} squareCoordinates The area where the Game Board will be drawn 
-     * @param {string} playerName The num of tile in x axis
+     * @param {string} playerColor The playercolor
      * @param {ImageData}   image The token background image 
      * @param {number}       size The token size 
      * @param {CanvasRenderingContext2D} ctx The context where the game board should be drawn;
      */
-    constructor(squareCoordinates, playerName, image, size, ctx) {
+    constructor(squareCoordinates, playerColor, image, size, ctx) {
         this.#squareCoordinates = squareCoordinates;
-        this.#playerName = playerName;
+        this.#playerColor = playerColor;
         this.#image      = image;
         this.#size       = size;
         this.#used       = false;
         this.#ctx        = ctx;
-        this.#x = 0; // un valor, puede ser aleatorio, dentro de squareCoordinates 
-        this.#y = 0; // un valor, puede ser aleatorio, dentro de squareCoordinates 
+        this.#x = Math.random() * (this.#squareCoordinates.x2 - this.#size*2) + (this.#squareCoordinates.x1 + this.#size)
+        this.#y = Math.random() * (this.#squareCoordinates.y2 - this.#size*2) + (this.#squareCoordinates.y1 + this.#size)
     }
 
     /**
      * Draw the token
      */
-    draw() { }
+    draw() {
+        this.#ctx.beginPath()
+        this.#ctx.arc(this.#x, this.#y, this.#size, 0, Math.PI*2, false)
+        this.#ctx.fillStyle = this.#playerColor
+        this.#ctx.fill()
+        this.#ctx.lineWidth = "2px"
+        this.#ctx.strokeStyle = "black"
+        this.#ctx.stroke()
+        this.#ctx.drawImage(this.#image, this.#x-this.#size, this.#y-this.#size, this.#size*2, this.#size*2)
+        this.#ctx.closePath()
+    }
 
     /**
      * indicates if the figure has already been used on the board 
      * @return {boolean} a boolean
      */
-    isUsed() {  }
+    isUsed() { return this.#used }
+
+    
+    /**
+     * Set the boolean that indicates whether the shape has already been used on the board
+     * @param {boolean} isUsed the boolean
+     */
+    setUsed(isUsed) { return this.#used = isUsed }
 
     /**
      * Set the y position of the token 
      * @param {number} y The y position of the token 
      */
-    set setX(x) {  }
+    set setX(x) { this.#x = x }
 
     /**
      * Set the y position of the token 
      * @param {number} y The y position of the token 
      */
-    set setY(y) {  }
+    set setY(y) { this.#y = y }
 
     /**
      * Get the x position of the token 
      * @return {number} The x position of the token 
      */
-    get x() {  }
+    get x() { return this.#x }
 
     /**
      * Get the y position of the token 
      * @return {number} The y position of the token 
      */
-    get y() {  }
+    get y() { return this.#y }
 
 }
