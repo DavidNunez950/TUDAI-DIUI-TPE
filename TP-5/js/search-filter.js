a2c6bd209461b57b8564fa7775cbe879ce3aa9ba
DavidNunez950 <davidlanuez@gmail.com>
2021-12-01 15:51:23 -0300

    Realizado parte de la busqueda avanzada

---
+++ TP-5/js/search-filter.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const select       = document.querySelector(".select select");
+    const selectText   = document.querySelector(".select-text");
+    const selectNumber = document.querySelector(".select-number");
+    select.addEventListener("click", (e) => {
+        if (e.target.tagName === "OPTION") {
+            let type = e.target.getAttribute("data-type");
+            select.setAttribute("data-type-selected", type)
+            if(type === "text") {
+                selectText.classList.remove("d-none")
+                selectNumber.classList.add("d-none")
+            } else {
+                selectText.classList.add("d-none")
+                selectNumber.classList.remove("d-none")
+            }
+        }
+    });
+
+    const filterContainer = document.querySelector(".filters-container");
+    const addFilter = document.querySelector(".search-filter");
+    const btnAdd = addFilter.querySelector("button");
+    const input = addFilter.querySelector("input");
+    btnAdd.addEventListener("click", () => {
+        if(input.value !== "") {
+            const operation = ((!selectText.classList.contains("d-none"))? selectText : selectNumber).querySelector("select");
+              
+            let filter = 
+            `<div class="p-10 bg-color-blanco rounded-1 d-flex flex-row align-items-center m-l-10 m-t-10">
+                <p class="font-body-2 font-color-negro p-r-10">${ select.selectedOptions["0"].innerText } <b class="font-color-gris-3">${ operation.selectedOptions["0"].innerText }</b> ${input.value}</p>
+                <i class="fas fa-times-circle icon-size-3"></i>
+            </div>`;
+            filterContainer.innerHTML +=filter;
+        }
+    });
+    filterContainer.addEventListener("click", (e)=> {
+        if (e.target.tagName === "I" && e.target.classList.contains("fa-times-circle")) {
+            filterContainer.removeChild(e.target.parentElement);
+        }
+    });
+
+
+
+    var modal = document.getElementById("myModal");
+
+    var btn = document.getElementById("myBtn");
+
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+    modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+});
